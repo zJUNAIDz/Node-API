93bd1f97ffdb29b5dd8dc6d06ff483e81ad7daf7
Junaid Shaikh <junaidshaikh3967@gmail.com>
2023-12-30 19:41:14 +0530

    Patch: update code to match new version of Joi

--- models/Genres.js
+++ models/Genres.js
@@ -1,20 +1,21 @@
 const Joi = require("joi");
 const mongoose = require("mongoose");
 const schema = new mongoose.Schema({
-  id: { type: Number, required: true },
+  // id: { type: Number, required: true },
   name: String,
-  description: String,
+  // description: String,
 });
 
 const Genre = mongoose.model("Genre", schema);
 
 function validateGenre(genre) {
-  const schema = {
+  const schema = Joi.object({
     name: Joi.string().min(1).required(),
-  };
-  return Joi.valid(genre, schema);
+  });
+
+  return schema.validate(genre);
 }
 
-exports.Genre = Genre;
-exports.validateGenre = validateGenre;
+module.exports.Genre = Genre;
+module.exports.validateGenre = validateGenre;
 // export default mongoose.model("Genre", schema);
