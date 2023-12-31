f9215d4e59db6f48b3d923604a822b5a79d94d2f
Junaid Shaikh <junaidshaikh3967@gmail.com>
2023-12-31 14:42:55 +0530

    patch: error handling using try catch block

--- routes/customers.js
+++ routes/customers.js
@@ -10,12 +10,13 @@ const router = express.Router();
  * @returns list of existing Customers in DB
  */
 router.get("/", async (req, res) => {
-  const customers = await Customer.find().sort("name");
-  if (!customers) {
+  try {
+    const customers = await Customer.find().sort("name");
+    res.send(customers);
+  } catch (err) {
     res.status(404).send("No customer yet");
     return;
   }
-  res.send(customers);
 });
 
 /**
@@ -23,12 +24,13 @@ router.get("/", async (req, res) => {
  * @returns JSON object of cusotomer with given Id
  */
 router.get("/:id", async (req, res) => {
-  const customer = await Customer.findById(req.params.id);
-  if (!customer) {
+  try {
+    const customer = await Customer.findById(req.params.id);
+    res.send(customer);
+  } catch (err) {
     res.status(404).send("Customer with given Id doesn't exist");
     return;
   }
-  res.send(customer);
 });
 
 /**
@@ -41,6 +43,7 @@ router.post("/", async (req, res) => {
     res.status(500).send(error.details[0].message);
     return;
   }
+
   const customer = new Customer(data);
   customer.save(); //* no await needed as we're not storing and using it somewhere
   res.send(customer);
@@ -51,33 +54,32 @@ router.post("/", async (req, res) => {
  * @returns Updated JSON object
  */
 router.put("/:id", async (req, res) => {
-  const prevCustomer = await Customer.findById(req.params.id);
-  const { isGold, name, phone } = {
-    isGold: req.body.isGold || prevCustomer.isGold,
-    name: req.body.name || prevCustomer.name,
-    phone: req.body.phone || prevCustomer.phone,
-  };
-  if (!prevCustomer) {
-    res.status(404).send("Customer with given Id doesn't exist");
-    return;
-  }
-  const customer = await Customer.findByIdAndUpdate(req.params.id, {
-    isGold,
-    name,
-    phone,
-  });
+  try {
+    const prevCustomer = await Customer.findById(req.params.id);
+    const { isGold, name, phone } = {
+      isGold: req.body.isGold || prevCustomer.isGold,
+      name: req.body.name || prevCustomer.name,
+      phone: req.body.phone || prevCustomer.phone,
+    };
+    const customer = await Customer.findByIdAndUpdate(req.params.id, {
+      isGold,
+      name,
+      phone,
+    });
 
-  res.send(customer);
+    res.send(customer);
+  } catch (err) {
+    res.status(404).send("Customer with given Id doesn't exist");
+  }
 });
 
 router.delete("/:id", async (req, res) => {
-  const customer = await Customer.findByIdAndDelete(req.params.id);
-  if (!customer) {
+  try {
+    const customer = await Customer.findByIdAndDelete(req.params.id);
+    res.send(customer);
+  } catch (err) {
     res.status(404).send("Customer with given Id doesn't exist");
-    return;
   }
-
-  res.send(customer);
 });
 
 module.exports = router;
