9e65f76f0e72708b8cd4df3cce47ea3662896b5d
Junaid Shaikh <junaidshaikh3967@gmail.com>
2023-12-31 12:47:45 +0530

    Add - route listener for customers

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
+
+const home = require("./routes/home");
 const genres = require("./routes/genres");
+const customers = require("./routes/customers");
 const express = require("express");
 const app = express();
 
@@ -9,7 +12,8 @@ mongoose
   .catch((err) => console.error("Failed to connect to MongoDB :("));
 
 app.use(express.json());
+app.use("/", home);
 app.use("/api/genres", genres);
-
+app.use("/api/customers", customers);
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening to port ${port}...`));
