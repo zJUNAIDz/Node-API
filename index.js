bf3542930b77e95edc363c8adfcd5d97a2082cc3
Junaid Shaikh <junaidshaikh3967@gmail.com>
2024-01-06 23:49:45 +0530

    fic: home route set view engine

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ const mongoose = require("mongoose");
 const home = require("./routes/home");
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
+const movies = require("./routes/movies");
 const express = require("express");
 const app = express();
-
 mongoose
   .connect("mongodb://localhost:27017/anime")
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Failed to connect to MongoDB :("));
 
 app.use(express.json());
+app.set("view engine", "pug");
 app.use("/", home);
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
+app.use("/api/movies", movies);
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening to port ${port}...`));
