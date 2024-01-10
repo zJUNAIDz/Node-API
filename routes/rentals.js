7b8e126593ebc932f988fb1971e405a18f83f969
Junaid Shaikh <junaidshaikh3967@gmail.com>
2024-01-10 23:49:37 +0530

    add: integrity in mongoDB using fawn

--- routes/rentals.js
+++ routes/rentals.js
@@ -1,9 +1,11 @@
 const { Customer } = require('../models/customer');
 const { Movie } = require('../models/movie');
 const { Rental, validate } = require('../models/rental');
-const express = require('express')
+const mongoose = require('mongoose')
+const Fawn = require('fawn')
+const express = require('express');
 const router = express.Router();
-
+Fawn.init("mongodb://localhost:27017/anime");
 
 router.get('/', async (req, res) => {
   const rentals = await Rental.find();
@@ -12,19 +14,52 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
   const { error, value } = validate(req.body)
-  if (error) {
-    res.send("request invalid");
-    return;
-  }
+  if (error) return res.status(400).send("Invalid request")
   const customer = await Customer.findById(value.customerId);
-  if (!customer) return res.send("Invalid Customer");
+  if (!customer) return res.status(400).send("Invalid Customer");
   const movie = await Movie.findById(value.movieId);
-  if (!movie) return res.send("Invlaid Movie")
-  const rental = await Rental({
-    customer,
-    movie,
-  });
-  res.send(rental);
+  if (!movie) return res.status(400).send("Invlaid Movie")
+  // const rental = await Rental({
+  //   customer, 
+  //   movie,
+  // });
+  const rental = new Rental({
+    customer: {
+      _id: customer._id,
+      name: customer.name,
+      phone: customer.phone,
+    },
+    movie: {
+      _id: movie._id,
+      title: movie.title,
+      dailyRentalRate: movie.dailyRentalRate,
+    }
+  })
+  //* THis code is not Intigirity friendly..In fact, there is no such thing as intigrity tho :D
+  // const result = rental.save();
+
+  // //* Updating number instock as someone juct got one now  
+  // movie.numberInStock--;
+  // movie.save();
+  // res.send(result);
+
+
+  //* Intigrity-friendly 
+  try {
+    const result = new Fawn.Task()
+      .save('rentals', rental) //* rentals (case-sensitive) is collection name in mongDB
+      .update('movies', { _id: movie._id }, {
+        $inc: {
+          numberInStock: -1
+        }
+      })
+      .run();
+    res.send(result);
+  } catch (err) {
+    //* in case if anyone of operation fails
+    res.status(500).send("Something went wrong with Intigrity...")
+  }
+
 })
 
 module.exports = router;
